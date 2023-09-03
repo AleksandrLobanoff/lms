4972bb120dd3fed65b8621ad897c47e639c620b6
Alexandp L <alexandp83@mail.ru>
2023-09-03 21:24:38 +0300

    созданы модели Course and Lesson

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    """Информация о курсе"""
+    tittle = models.CharField(max_length=100, verbose_name='название курса')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='course/', verbose_name='превью', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.tittle}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    """Информация об уроке"""
+    tittle = models.CharField(max_length=100, verbose_name='название урока')
+    preview = models.ImageField(upload_to='lesson/', verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание')
+    video_link = models.TextField(verbose_name='ссылка на видео')
+
+    def __str__(self):
+        return f'{self.tittle}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
